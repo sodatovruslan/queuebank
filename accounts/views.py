8baff8556dbaf72b3c0127eef485e4687d3deefb
sodatovruslan <email@github.com>
2026-05-25 17:46:22 +0300

    add logout view

--- accounts/views.py
+++ accounts/views.py
@@ -30,4 +30,8 @@ def login_view(request):
     return render(request, 'accounts/login.html')
 
 
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
 # Create your views here.
