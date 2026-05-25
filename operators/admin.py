3e27817be45cb49953db6fa5d13b7fdd729e29fc
sodatovruslan <email@github.com>
2026-05-25 08:52:40 +0300

    add models in admin py

--- operators/admin.py
+++ operators/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Operator
 
+admin.site.register(Operator)
 # Register your models here.
--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Service, Window, Ticket
 
+admin.site.register(Service)
+admin.site.register(Window)
+admin.site.register(Ticket)
 # Register your models here.
