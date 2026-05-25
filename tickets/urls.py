a8bed51b69e9acfec46f1b32215c69b544223e1c
sodatovruslan <email@github.com>
2026-05-25 10:55:35 +0300

    Add operator group and permissions

--- tickets/urls.py
+++ tickets/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('take/<int:service_id>/', views.take_ticket, name='take_ticket'),
-    path('my-ticket/', views.my_ticket, name='my_ticket'),
+   #  path('', views.home, name='home'),
+   #  path('take/<int:service_id>/', views.take_ticket, name='take_ticket'),
+   #  path('my-ticket/', views.my_ticket, name='my_ticket'),
 ]
