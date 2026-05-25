b3abe2b29dae422ba3ed8e77edfcc5dcbc86529a
sodatovruslan <email@github.com>
2026-05-25 17:42:20 +0300

    add register in accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Client
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+        Client.objects.create(user=user, full_name=username, phone='')
+        return redirect('login')
+    return render(request, 'accounts/register.html')
 
 # Create your views here.
