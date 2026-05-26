95e568dd3c6648ff5eb4763604628df51f5983ea
sodatovruslan <email@github.com>
2026-05-26 16:33:47 +0300

    Fix done and skip ticket urls

--- operators/urls.py
+++ operators/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('panel/', views.operator_panel, name='operator_panel'),
     path('next/', views.next_client, name='next_client'),
-    path('done/<int:ticket_id>/', views.done_ticket, name='done_ticket'),
-    path('skip/<int:ticket_id>/', views.skip_ticket, name='skip_ticket'),
+    path('done/<int:pk>/', views.done_ticket, name='done_ticket'),
+    path('skip/<int:pk>/', views.skip_ticket, name='skip_ticket'),
 ]
