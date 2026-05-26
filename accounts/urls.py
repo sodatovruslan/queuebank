50751a97e4d37cdacc6f476efd60099716febb38
sodatovruslan <email@github.com>
2026-05-26 08:35:39 +0300

    change urls py acccounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('confirm-email/', views.confirm_email, name='confirm_email'),
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+    path('reset-confirm/', views.reset_confirm, name='reset_confirm'),
 ]
