e92f964f079fecdfbb1f19892431a0aa29df5b63
sodatovruslan <email@github.com>
2026-05-26 08:38:20 +0300

    Add home view

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Service, Ticket, Window
 
+def home(request):
+    services = Service.objects.filter(is_active=True)
+    return render(request, 'tickets/home.html', {'services': services})
 # Create your views here.
