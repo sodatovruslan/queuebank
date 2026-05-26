c744968e114cd1a0e80666641e4effbfc685e7e0
sodatovruslan <email@github.com>
2026-05-26 15:58:02 +0300

    Register EmailConfirm in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client, EmailConfirm
 
 admin.site.register(Client)
+admin.site.register(EmailConfirm)
 # Register your models here.
