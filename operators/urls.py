61fcaeccd1c530601fadebd5e173c35433931f30
sodatovruslan <email@github.com>
2026-05-28 08:47:10 +0300

    Add toggle_window url

--- operators/urls.py
+++ operators/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('next/', views.next_client, name='next_client'),
     path('done/<int:pk>/', views.done_ticket, name='done_ticket'),
     path('skip/<int:pk>/', views.skip_ticket, name='skip_ticket'),
+    path('toggle-window/', views.toggle_window, name='toggle_window'),
 ]
