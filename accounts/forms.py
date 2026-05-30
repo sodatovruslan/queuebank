3b7c00ee0677075cbae4209347e0723a61061119
sodatovruslan <email@github.com>
2026-05-30 07:15:47 +0300

    add login forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,3 +14,12 @@ class RegisterForm(forms.Form):
     password2 = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'})
     )
+    
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Username or Email'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'})
+    )
